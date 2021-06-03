a6ce02fdaa739841440bf754f0766d37b9ddf49a
janhavi07 <56464138+janhavi07@users.noreply.github.com>
2021-06-04 02:24:37 +0530

    [Janhavi]Edit functional Test code.

--- Application/aspnet-core-dotnet-core.FunctionalTests/SampleFunctionalTests.cs
+++ Application/aspnet-core-dotnet-core.FunctionalTests/SampleFunctionalTests.cs
@@ -44,7 +44,7 @@ namespace SampleWebApplication.FunctionalTests
                 try
                 {
                     driver.Navigate().GoToUrl(webAppUrl);
-                    Assert.AreEqual("Home Page - ASP.NET Core", driver.Title, "Expected title to be 'Home Page - ASP.NET Core'");
+                    Assert.AreEqual("parkingtrail.azurewebsites.net", driver.Title, "Expected title to be 'Home Page - ASP.NET Core'");
                     break;
                 }
                 catch(Exception e)
